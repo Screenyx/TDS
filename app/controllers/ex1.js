486bbe2f1e557e418b74edf4f9a930e9a7236203
Hugo Mercier <mercier.hugo14@gmail.com>
2021-01-26 16:17:43 +0100

    ajout du bouton note modifie qui pop quand on save et qui disparait quand on supp

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -3,20 +3,43 @@ import {action} from '@ember/object';
 import {tracked} from "@glimmer/tracking";
 
 export default class Ex1Controller extends Controller {
-    @tracked content = 'valeur par défaut';
+    @tracked content = '';
     @tracked info ='';
     
     MAX=100;
+
+    get style(){
+        if(this.size > 50){
+            return 'info';
+        }
+        if(this.size > 20){
+            return 'warning';
+        }
+        return 'danger';
+
+    }
+
 
     get size(){
         return this.MAX - this.content.length;
     }
 
     @action save(){
-        console.log('Test save '+this.content)
+        console.log('Test save '+this.content);
+        
     }
 
     @action supprimer(){
-        this.content = ''
+        this.content = '';
+    }
+
+    @action update(){
+        if(this.content !=''){
+            this.info = "Note modifiée";
+        }
+    }
+
+    @action clear(){
+       this.info ='';     
     }
 }
